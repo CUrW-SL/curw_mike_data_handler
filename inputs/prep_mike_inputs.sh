55174d696e37a8cc05b625db18fcfefb96b41eef
shadhini <jshadhiniaseka@gmail.com>
2020-05-20 13:52:49 +0530

    implement runner to prepapre mike inputs hourly

---
+++ inputs/prep_mike_inputs.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+echo `date`
+
+echo "Changing into ~/curw_mike_data_handler"
+cd /home/uwcc-admin/curw_mike_data_handler
+echo "Inside `pwd`"
+
+
+# If no venv (python3 virtual environment) exists, then create one.
+if [ ! -d "venv" ]
+then
+    echo "Creating venv python3 virtual environment."
+    virtualenv -p python3 venv
+fi
+
+# Activate venv.
+echo "Activating venv python3 virtual environment."
+source venv/bin/activate
+
+# Install dependencies using pip.
+if [ ! -f "mike_utils.log" ]
+then
+    echo "Installing PyMySQL"
+    pip install PyMySQL
+    echo "Installing PyYAML"
+    pip install PyYAML
+    echo "Installing db adapter"
+    pip install git+https://github.com/shadhini/curw_db_adapter.git
+    touch mike_utils.log
+fi
+
+# prepare mike input rain
+echo "Preparing input rain for mike ..."
+./inputs/gen_mike_input_rf_linux.py >> inputs/mike_input_rain.log 2>&1
+
+# prepare mike input discharge
+echo "Preparing input discharge for mike ..."
+./inputs/gen_mike_input_dis_linux.py >> inputs/mike_input_dis.log 2>&1
+
+# Deactivating virtual environment
+echo "Deactivating virtual environment"
+deactivate
