6abbd255b44ec4044475920f387fd26fe8d8c46f
shadhini <jshadhiniaseka@gmail.com>
2020-05-21 02:18:15 +0530

    add script to prepare tide input for mike

--- inputs/gen_mike_input_tide_linux.py
+++ inputs/gen_mike_input_tide_linux.py
@@ -85,24 +85,24 @@ def replace_negative_numbers_with_nan(df):
     return df
 
 
-def prepare_mike_dis_input(TS, start, end, dis_id):
+def prepare_mike_dis_input(start, end, tide_id):
 
     try:
+        pool = get_Pool(host=con_params.CURW_SIM_HOST, port=con_params.CURW_SIM_PORT, user=con_params.CURW_SIM_USERNAME,
+                        password=con_params.CURW_SIM_PASSWORD,
+                        db=con_params.CURW_SIM_DATABASE)
+        TS = Timeseries(pool)
 
-        dis_ts_df = pd.DataFrame()
-        dis_ts_df['time'] = pd.date_range(start=start, end=end, freq='15min')
-
-        ts = TS.get_timeseries(id_=dis_id, start_date=start, end_date=end)
-        ts.insert(0, ['time', 'value'])
+        ts = TS.get_timeseries(id_=tide_id, start_date=start, end_date=end)
         ts_df = list_of_lists_to_df_first_row_as_columns(ts)
 
-        dis_ts_df = pd.merge(dis_ts_df, ts_df, how="left", on='time')
+        ts_df.set_index('time', inplace=True)
 
-        dis_ts_df.set_index('time', inplace=True)
+        ts_df = replace_negative_numbers_with_nan(ts_df)
 
-        dis_ts_df.fillna(method='ffill').fillna(method='bfill')
+        ts_df.dropna()
 
-        return dis_ts_df
+        return ts_df
 
     except Exception:
         traceback.print_exc()
@@ -112,7 +112,7 @@ def prepare_mike_dis_input(TS, start, end, dis_id):
 
 def usage():
     usageText = """
-    Usage: .\gen_mike_input_rf.py [-s "YYYY-MM-DD HH:MM:SS"] [-e "YYYY-MM-DD HH:MM:SS"]
+    Usage: .\gen_mike_input_tide_linux.py [-s "YYYY-MM-DD HH:MM:SS"] [-e "YYYY-MM-DD HH:MM:SS"]
 
     -h  --help          Show usage
     -s  --start_time    Mike rainfall timeseries start time (e.g: "2019-06-05 00:00:00"). Default is 00:00:00, 3 days before today.
@@ -146,12 +146,12 @@ if __name__ == "__main__":
                 end_time = arg.strip()
 
         # Load config params
-        config = json.loads(open(os.path.join('inputs', 'dis_config.json')).read())
+        config = json.loads(open(os.path.join('inputs', 'tide_config.json')).read())
 
         output_dir = read_attribute_from_config_file('output_dir', config)
         file_name = read_attribute_from_config_file('output_file_name', config)
 
-        dis_id = read_attribute_from_config_file('dis_id', config)
+        tide_id = read_attribute_from_config_file('tide_id', config)
 
         if start_time is None:
             start_time = (datetime.now() - timedelta(days=3)).strftime('%Y-%m-%d 00:00:00')
@@ -163,29 +163,22 @@ if __name__ == "__main__":
         else:
             check_time_format(time=end_time)
 
-        pool = get_Pool(host=con_params.CURW_SIM_HOST, port=con_params.CURW_SIM_PORT, user=con_params.CURW_SIM_USERNAME,
-                        password=con_params.CURW_SIM_PASSWORD,
-                        db=con_params.CURW_SIM_DATABASE)
-        TS = Timeseries(pool)
-
-        latest_fgt = TS.get_obs_end(dis_id)
-
         if output_dir is None:
             output_dir = os.path.join(OUTPUT_DIRECTORY, (datetime.utcnow() + timedelta(hours=5, minutes=30)).strftime('%Y-%m-%d_%H-00-00'))
         if file_name is None:
-            file_name = 'mike_dis_{}.txt'.format(latest_fgt.strftime('%Y-%m-%d_%H-%M-00'))
+            file_name = 'mike_tide.txt'
 
-        mike_dis_file_path = os.path.join(output_dir, file_name)
+        mike_tide_file_path = os.path.join(output_dir, file_name)
 
-        if not os.path.isfile(mike_dis_file_path):
-            makedir_if_not_exist_given_filepath(mike_dis_file_path)
+        if not os.path.isfile(mike_tide_file_path):
+            makedir_if_not_exist_given_filepath(mike_tide_file_path)
             print("{} start preparing mike rainfall input".format(datetime.now()))
-            mike_discharge = prepare_mike_dis_input(TS=TS, start=start_time, end=end_time, dis_id=dis_id)
-            mike_discharge.to_csv(mike_dis_file_path, header=False, index=True)
+            mike_discharge = prepare_mike_dis_input(start=start_time, end=end_time, tide_id=tide_id)
+            mike_discharge.to_csv(mike_tide_file_path, header=False, index=True)
             print("{} completed preparing mike rainfall input".format(datetime.now()))
-            print("Mike input rainfall file is available at {}".format(mike_dis_file_path))
+            print("Mike input rainfall file is available at {}".format(mike_tide_file_path))
         else:
-            print('Mike rainfall input file already in path : ', mike_dis_file_path)
+            print('Mike rainfall input file already in path : ', mike_tide_file_path)
 
     except Exception:
         traceback.print_exc()
