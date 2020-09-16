d08b21e5c5208841e4fd514ac263da67e1822af4
shadhini <jshadhiniaseka@gmail.com>
2020-09-17 00:20:21 +0530

    wrapping up implementation

--- inputs/gen_mike_input_rf_obs_win.py
+++ inputs/gen_mike_input_rf_obs_win.py
@@ -159,18 +159,18 @@ def prepare_mike_rf_input(start, end, step):
         ts_df.set_index('time', inplace=True)
 
         mike_input = replace_negative_numbers_with_nan(ts_df)
-        pd.set_option('display.max_rows', mike_input.shape[0] + 1)
-        pd.set_option('display.max_columns', mike_input.shape[1] + 1)
-        print(mike_input)
-        print("#######################################")
+        # pd.set_option('display.max_rows', mike_input.shape[0] + 1)
+        # pd.set_option('display.max_columns', mike_input.shape[1] + 1)
+        # print(mike_input)
+        # print("#######################################")
         # mike_input = replace_nan_with_empty_string(mike_input)
 
         mike_input = mike_input.fillna('')
         mike_input = mike_input.round(1)
-        pd.set_option('display.max_rows', mike_input.shape[0] + 1)
-        pd.set_option('display.max_columns', mike_input.shape[1] + 1)
-        print(mike_input)
-        print("#######################################")
+        # pd.set_option('display.max_rows', mike_input.shape[0] + 1)
+        # pd.set_option('display.max_columns', mike_input.shape[1] + 1)
+        # print(mike_input)
+        # print("#######################################")
 
         for col in mike_input.columns:
             mike_input = mike_input.rename(columns={col: station_dict.get(col)[1]})
