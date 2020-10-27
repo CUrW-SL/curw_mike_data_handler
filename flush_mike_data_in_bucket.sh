3a2ca0ebd384b2e7259067619e5895a9d9f4cb60
shadhini <jshadhiniaseka@gmail.com>
2020-10-27 20:55:41 +0530

    use gsutil instead of linux file handling utils to flush mike data in bucket

--- flush_mike_data_in_bucket.sh
+++ flush_mike_data_in_bucket.sh
@@ -1,29 +1,16 @@
+files=$(gsutil ls gs://wrf_nfs/mike/inputs/*/*.txt)
+files+=$(gsutil ls gs://wrf_nfs/mike/outputs/*/*.*)
+
 NOW=$(date +%s)
 
-cd /mnt/disks/wrf_nfs/mike/inputs
-
-for dir in $(ls -d */)
+for file in $files;
 do
-  LAST_MODIFIED_DATE=$(date -r $dir +%s)
+  LAST_MODIFIED_DATE=$(date -r $file +%s)
   DIFF=$(((NOW-LAST_MODIFIED_DATE)/60/60/24))
   echo $DIFF
   if [ $DIFF -gt 60 ]
   then
-    echo $dir
-    rm -vr $dir
-  fi
-done
-
-cd /mnt/disks/wrf_nfs/mike/outputs
-
-for dir in $(ls -d */)
-do
-  LAST_MODIFIED_DATE=$(date -r $dir +%s)
-  DIFF=$(((NOW-LAST_MODIFIED_DATE)/60/60/24))
-  echo $DIFF
-  if [ $DIFF -gt 60 ]
-  then
-    echo $dir
-    rm -vr $dir
+    echo $file
+    rm -vr $file
   fi
 done
