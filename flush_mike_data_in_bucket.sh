bda73003d9773c5f1014b935e25cc3e3527a3e3d
shadhini <jshadhiniaseka@gmail.com>
2020-10-28 08:17:40 +0530

    use 2 lists for 2 file sets

--- flush_mike_data_in_bucket.sh
+++ flush_mike_data_in_bucket.sh
@@ -1,5 +1,29 @@
 files=$(gsutil ls gs://wrf_nfs/mike/inputs/*/*.txt)
-files+=$(gsutil ls gs://wrf_nfs/mike/outputs/*/*.*)
+
+NOW=$(date +%s)
+
+for file in $files;
+do
+  echo $file
+
+  IFS='/' read -ra LIST <<< "$file"
+
+  filename="/mnt/disks"
+  for i in "${LIST[@]:2}"; do
+    filename="$filename/$i"
+  done
+  echo $filename
+
+  LAST_MODIFIED_DATE=$(date -r $filename +%s)
+  DIFF=$(((NOW-LAST_MODIFIED_DATE)/60/60/24))
+  echo $DIFF
+  if [ $DIFF -gt 60 ]
+  then
+    rm -v $filename
+  fi
+done
+
+files=$(gsutil ls gs://wrf_nfs/mike/outputs/*/*.*)
 
 NOW=$(date +%s)
 
